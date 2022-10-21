514de782f2fe2701d9edba4f31f02fac4faca042
DenzOjo <116380952+DenzOjo@users.noreply.github.com>
2022-10-21 18:56:56 +0100

    Add files via upload

---
+++ Month1 Denzel.py
@@ -0,0 +1,44 @@
+#Create 5 variables
+name='Denzel'
+name2=1
+parent=1.5
+parent2=True
+baby=2
+
+#Output
+print(name)
+print(name2)
+print(parent)
+print(parent2)
+print(baby)
+
+#Types
+print(type(name))
+print(type(name2))
+print(type(parent))
+print(type(parent2))
+print(type(baby))
+#creating 2 variables
+student='Denzel'
+student2='Olivia'
+#methods
+print(student.upper())
+print(student2.lower())
+print(student.title())
+
+
+#arithmetic operations
+print(9 + 6)
+print(1-7)
+print(3*8)
+print(10/2)
+print(9//3)
+print(5%3)
+print(5**3)
+
+#methods and placeholder
+first_name ='denzel'
+last_name ='ojo'
+full_name =f'{first_name} {last_name}'
+message =f'hello {full_name.title()}'
+print(message)
