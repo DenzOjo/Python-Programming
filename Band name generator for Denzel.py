27beffd7d75a9b634479ca7aec9888c688c69fa4
DenzOjo <116380952+DenzOjo@users.noreply.github.com>
2022-12-09 19:27:35 +0000

    Add files via upload

---
+++ Band name generator for Denzel.py
@@ -0,0 +1,9 @@
+#Band Name generator
+print('welcome to the Band Name generator')
+print()
+colour= input ('what is your favourite colour ? ')
+print (f'nice your favourite colour is {colour}')
+animal= input ('what is your favourite animal ? ')
+print (f'fine your favourite animal is {animal}')
+print()
+print(f'your band name is \n{colour.upper()} {animal.upper()}')
