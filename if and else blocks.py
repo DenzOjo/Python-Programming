c2a577b942115daa22da1bbc2a0e7dc5d48e5db2
DenzOjo <116380952+DenzOjo@users.noreply.github.com>
2023-02-02 20:00:05 +0000

    Add files via upload

---
+++ if and else blocks.py
@@ -0,0 +1,20 @@
+name=input('sup what\'s your name: ')
+print(f'sup {name} that\'s a COOL name')
+
+
+hobby=input('what\'s your hobby')
+if hobby=="coding":
+  print('wow that\'s my favourite hobby too!')
+else:
+  print(f'uhh.. i\'m sorry i dont know what "{hobby}" is')
+
+
+game=input('what\'s your favourite game')
+if game=="minecraft":
+  print('nice that\'s my favourite game too!')
+else:
+  print(f'oh sorry i\'ve never played "{game}" before')
+
+
+tv_show=input('what\'s your favourite tv_show')
+print('thats a nice tv show i can\'t really watch TV shows because i\'m an AI')
