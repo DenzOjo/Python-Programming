559965538ca5f7751270092ed7410deda4bbda97
DenzOjo <116380952+DenzOjo@users.noreply.github.com>
2023-02-02 17:58:37 +0000

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+'''
+It was ___(FOOD)___ day at school, and ___(NAME)___ was super ___(ADJECTIVE)___ for lunch.
+But when she went outside to eat, a ___(Animal)___ stole her ___(FOOD)___! ___(NAME)___ chased the ___(animal)___ all over school.
+She ___(VERB1 past tense)___, ___(VERB2 past tense)___, and ___(VERB3 past tense)___ through the playground.
+Then she tripped and the ___(animal)___ escaped!
+Luckily, ___(NAME)___’s friends were willing to share their ___(FOOD)___ with her.
+'''
+
+name=input('enter a name:\n')
+print()
+food=input('enter a food:banana,piece of meat\n')
+print()
+animal=input('enter an animal:\n')
+print()
+adjective=input('enter an adjective:ferocious,scary,dangerous\n')
+print()
+verb1=input('enter a verb:jumped,appeared\n')
+print()
+verb2=input('enter a verb:ran,scampered\n')
+print()
+verb3=input('finally enter one more verb:jogged,skipped\n')
+
+print(f'''one day {name} was walking in the forest when suddenly \na {adjective} looking {animal} {verb1} out of no where {name} was\nscared but gave it a {food} and the {animal} {verb2} away after\nthat {name} {verb3} all the way home''')
